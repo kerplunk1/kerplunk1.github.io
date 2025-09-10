3fc6257485d867b3a46b4daf815df145e3b4650e
cynepsonic <cynepsonic@gmail.com>
2025-09-10 14:44:22 +0400

    initial commit

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,7 +2,7 @@ import './AboutMe.css'
 
 export default function AboutMe() {
   return (
-    <div className="info">
+    <div className="about-me">
       <p>Мне 33 года, и у меня разносторонний опыт в IT: от технической поддержки и системного администрирования до разработки веб-приложений и автоматизации рутинных задач. Последний год активно развиваюсь в направлении разработки. Пишу на Python (Django, FastAPI), немного работаю с фронтендом на JavaScript (React), использую PostgreSQL, Git и Docker.</p>
     </div>      
   );
